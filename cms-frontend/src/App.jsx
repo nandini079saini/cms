bbf748405380416caddb08ba86dea2ad8f8ab355
Nandini <nandini.saini11193@gmail.com>
2026-06-28 22:36:50 +0530

    Public CMS frontend

---
+++ cms-frontend/src/App.jsx
@@ -0,0 +1,43 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import PostDetail from "./pages/PostDetail";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import useVisitTracker from "./hooks/useVisitTracker";
+import { useAuth } from "./context/useAuth";
+
+function AppInner() {
+  useVisitTracker(); // tracks every page visit
+  const { user } = useAuth();
+
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/" replace />}
+        />
+        <Route
+          path="/"
+          element={user ? <Home /> : <Navigate to="/login" replace />}
+        />
+        <Route
+          path="/post/:id"
+          element={user ? <PostDetail /> : <Navigate to="/login" replace />}
+        />
+
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </>
+  );
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <AppInner />
+    </BrowserRouter>
+  );
+}
